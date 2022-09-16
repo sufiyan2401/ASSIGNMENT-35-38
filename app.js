a8818ee5fbdc92e6da53b769e15b6e94e8153aac
Sufiyan <110626687+sufiyan2401@users.noreply.github.com>
2022-09-16 23:05:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+// 35 -- 38 //
+
+// ASSIGNMENT 1 //
+// var todaydate = new Date();
+// document.write(todaydate);
+
+// ASSIGNMENT 2 //
+// let a= prompt("Enter Your First Name")
+// let b= prompt("Enter Your Sur Name")
+// let c= a+ b
+// document.write(c)
+
+// ASSIGNMENT 3 //
+// var a =prompt("Enter First Value")
+// var b =prompt("Enter Second Value")
+// var c =Number(a)+Number(b)
+// alert(c)
+
+// ASSIGNMENT 4 //
+//  var a =prompt("Enter 1st value")
+//  var b =prompt("Enter 2nd value")
+//  let c =prompt("Enter An Operator i.e: +,-,*,/")
+//  if(c == "+"){
+    //      alert(Number(a)+Number(b))
+    //  }
+//  else if(c == "-"){
+    //      alert(Number(a)-Number(b))
+    //  }
+    //  else if(c == "*"){
+        //      alert(Number(a)*Number(b))
+        //  }
+        //  else if(c == "/"){
+            //      alert(Number(a)/Number(b))
+            //  }
+            // else if(c){
+                //      alert(You Inputed Wrong Values)
+                //  }
+
+ // ASSIGNMENT 5 //
+//  var a =prompt("Enter Your Value")
+//  var b = Number(a)*Number(a)
+//  alert(b);
+
+// ASSIGNMENT 6 //
+//Not nderstand
+
+// ASSIGNMENT 7 //
+// NOT UNDERSTAND //
+
+//ASSIGNMENT 8//
+// var a =prompt("Enter Your Base")
+// var b =prompt("Enter Your Perpendicular")
+// var z =Number(a)*Number(a)
+// var x =Number(b)*Number(b)
+// var m =Number(z)+Number(x)
+// alert("Your Hypotenuse is" + m )
+
+// ASSIGNMNET 9 //
+// var b =prompt("Enter Your Width")
+// var c =prompt("Enter Your height")
+// var m = Number(b)*Number(c)
+// alert("Your Area Is" + m)
+
+// ASSIGNMENT 10 //
+// Not done
+
+// ASSIGNMENT 11//
+// function uppercase(str)
+// {
+//   var array1 = str.split(' ');
+//   var newarray1 = [];
+    
+//   for(var x = 0; x < array1.length; x++){
+//       newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+//   }
+//   return newarray1.join(' ');
+// }
+// let me = prompt("Enter Your Text")
+// document.write(uppercase(me));
+
+
+// ASSIGNMENT 12 //
+// function find_longest_word(str)
+// {
+//   var array1 = str.match(/\w[a-z]{0,}/gi);
+//   var result = array1[0];
+
+//   for(var x = 1 ; x < array1.length ; x++)
+//   {
+//     if(result.length < array1[x].length)
+//     {
+//     result = array1[x];
+//     } 
+//   }
+//   return result;
+// }
+// let md = prompt("Enter Your Text")
+// document.write(find_longest_word(md));
+
+
+// ASSIGNMENT 13//
+// function char_count(str, letter) 
+// {
+//  var letter_Count = 0;
+//  for (var position = 0; position < str.length; position++) 
+//  {
+//     if (str.charAt(position) == letter) 
+//       {
+//       letter_Count += 1;
+//       }
+//   }
+//   return letter_Count;
+// }
+
+// alert(char_count('JSResourceS.com', 'o'));
+
+// ASSIGNMENT 14 //
+ 
+
+
+
